82be00925168ebd83cd31079a6058bb577255194
ashyrion <ashyric@naver.com>
2022-11-14 00:33:09 +0900

    remove ignored file

--- lib/api/api_key.dart
+++
@@ -1,1 +0,0 @@
-const apiKey = "617b64b7d26de5ce225fe0ec30e4c478";
--- lib/widgets/hourly_data_widget.dart
+++ lib/widgets/hourly_data_widget.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 import 'package:intl/intl.dart';
